a2a58be614ae42ae70c7ba517bc7f3308ae0f590
Ilya <mifomen@yandex.ru>
2021-09-17 21:44:43 +0300

    index 50\50

--- source/js/script.js
+++ source/js/script.js
@@ -8,12 +8,29 @@ if (document.querySelector(".js-menu-open")) {
 }
 
 
+// alert( window.innerWidth )
+// alert( document.documentElement.clientWidth )
+ var icon = "img/pin-on-map-mobile.svg"
+
+ if (window.innerWidth >= 320) {
+  icon = "img/pin-on-map-tablet.svg"
+} else {
+  icon = "img/pin-on-map-mobile.svg"
+}
+
+var center = {lat: 59.939885, lng: 30.323118}
+ if (window.innerWidth >= 768) {
+var center = {lat: 59.939885, lng: 30.313018}
+} else {
+  var center = {lat: 59.939885, lng: 30.323118}
+}
+
 
 let map;
 const coordinates = {  lat: 59.938635,  lng: 30.323118};
 function initMap() {
   map = new google.maps.Map(document.querySelector("#js-google-map"), {
-    center: { lat: 59.939885, lng: 30.323118 },
+    center: { lat: center.lat, lng: center.lng },
     zoom: 15,
   });
   new google.maps.Marker({
@@ -22,13 +39,15 @@ function initMap() {
     title: "Мы здесь!",
     // content:"<h1 class="blog-content-link">mifomen</h1>",
 
-    icon: "img/pin-on-map.svg",
+    // icon: "img/pin-on-map.svg",
+    icon: icon,
+    // icon: "default",
     //   url:
     //   scaledSize: (window.isMobile)
     //           ? new google.maps.Size(20, 34)
     //           : new google.maps.Size(40, 68)
     // }
-    draggable: false
+    draggable: false,
   });
 
   if (document.querySelector('.js-hide-map')) {
